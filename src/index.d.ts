79b015c559604e430c877f096918f04a5ceedc8e
Roberto Wesley Overdijk <r.w.overdijk@gmail.com>
2019-04-26 12:05:23 +0200

    Allow stateless functional component

--- src/index.d.ts
+++ src/index.d.ts
@@ -56,7 +56,7 @@ interface MessageOptions {
 interface FlashMessageProps extends Partial<MessageOptions> {
   canRegisterAsDefault?: boolean;
   style?: StyleProp<ViewStyle>;
-  MessageComponent?: React.ReactElement<MessageComponentProps>;
+  MessageComponent?: React.SFC<MessageComponentProps> | React.ReactElement<MessageComponentProps>;
   transitionConfig?(animValue: Animated.Value, position: Position): Transition;
   renderFlashMessageIcon?(
     icon: Icon,
